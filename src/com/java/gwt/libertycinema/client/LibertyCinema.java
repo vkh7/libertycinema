79c6c7267459b2afcef91c6178a8c0257f6ae580
Venkatesh Hodavdekar <vhodavdekar@gmail.com>
2012-05-24 09:46:12 -0400

    add some more buttons

--- src/com/java/gwt/libertycinema/client/LibertyCinema.java
+++ src/com/java/gwt/libertycinema/client/LibertyCinema.java
@@ -4,30 +4,46 @@ package com.java.gwt.libertycinema.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 
-import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import com.google.gwt.dom.client.Style.Unit;
 
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class LibertyCinema implements EntryPoint {
 
-    /**
-     * This is the entry point method.
-     */
+    private DockLayoutPanel main = new DockLayoutPanel(Unit.EM);
+    private VerticalPanel header = new VerticalPanel();
+    private HorizontalPanel headerHtml = new HorizontalPanel();
+    private HorizontalPanel headerButtons = new HorizontalPanel();
+    private HorizontalPanel body = new HorizontalPanel();
+    private HorizontalPanel footer = new HorizontalPanel();
+
     public void onModuleLoad() {
-	DockLayoutPanel p = new DockLayoutPanel(Unit.EM);
-	// Create a vertical panel, add buttons to north
-	// Create a vertical panel, add acks to the south
-	// Create a horizontal panel, add content to middle
-	p.addNorth(new HTML("header"), 2);
-	p.addSouth(new HTML("footer"), 2);
-	p.add(new HTML("asdjlkasdljadsassadass"));
-	RootLayoutPanel.get().add(p);
+	headerHtml.add(new HTML("<h2>Liberty Cinema</h2>"));
+
+	Button home = new Button("Home");
+	Button contactUs = new Button("Contact Us");
+
+	headerButtons.add(home);
+	headerButtons.add(contactUs);
+
+
+	header.add(headerHtml);
+
+	header.add(headerButtons);
+
+
+	main.addNorth(header, 7);
+
+	main.addSouth(new HTML("By Venkatesh Hodavdekar (vhodavdekar@gmail.com)"), 2);
+	main.add(new HTML("asd"));
+
+	RootLayoutPanel.get().add(main);
 
     }
 }
