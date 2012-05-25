ff3e8920071e28091d3e9fd974229b549588614e
Venkatesh Hodavdekar <vhodavdekar@gmail.com>
2012-05-24 21:52:58 -0400

    remove some additional variables

--- src/com/java/gwt/libertycinema/client/FooterBar.java
+++ src/com/java/gwt/libertycinema/client/FooterBar.java
@@ -1,20 +1,22 @@
 package com.java.gwt.libertycinema.client;
 
+
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 public class FooterBar {
+
     private DockLayoutPanel main;
+    private VerticalPanel panel = new VerticalPanel();
 
     public FooterBar(DockLayoutPanel main) {
 	this.main = main;
     }
 
     public void setUpFooter() {
-	HTML credits = new HTML("Copyright Liberty Cinema (2012)");
-	this.main.addSouth(credits, 2);
+	panel.add(new HTML("Copyright Liberty Cinema (2012)"));
+	this.main.addSouth(panel, 2);
     }
 }
