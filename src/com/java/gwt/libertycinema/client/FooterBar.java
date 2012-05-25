1f64d1083c5520b7106ef29ff90ee4613542c424
Venkatesh Hodavdekar <vhodavdekar@gmail.com>
2012-05-24 22:15:51 -0400

    don't use panels for footer

--- src/com/java/gwt/libertycinema/client/FooterBar.java
+++ src/com/java/gwt/libertycinema/client/FooterBar.java
@@ -3,20 +3,20 @@ package com.java.gwt.libertycinema.client;
 
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 
 
 public class FooterBar {
 
     private DockLayoutPanel main;
-    private VerticalPanel panel = new VerticalPanel();
+    private HorizontalPanel panel = new HorizontalPanel();
 
     public FooterBar(DockLayoutPanel main) {
 	this.main = main;
     }
 
     public void setUpFooter() {
-	panel.add(new HTML("Copyright Liberty Cinema (2012)"));
-	this.main.addSouth(panel, 2);
+	this.main.addSouth(new HTML("Copyright Liberty Cinema (2012)"), 2);
     }
 }
