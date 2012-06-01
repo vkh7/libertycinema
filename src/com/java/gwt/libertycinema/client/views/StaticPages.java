cf966e4a7f00ac4b77cc16186d072f2bf41883b2
Venkatesh Hodavdekar <mail.dizzy@gmail.com>
2012-05-31 22:06:58 -0400

    add staticpage skeleton

---
+++ src/com/java/gwt/libertycinema/client/views/StaticPages.java
@@ -0,0 +1,16 @@
+package com.java.gwt.libertycinema.client.views;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import com.java.gwt.libertycinema.client.BodyPanel;
+
+
+public class StaticPages {
+
+    private BodyPanel body;
+
+    public StaticPages(BodyPanel body) {
+	this.body = body;
+    }
+
+}
