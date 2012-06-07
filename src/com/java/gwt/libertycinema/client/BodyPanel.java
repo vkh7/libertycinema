f12aad531d910c6c237d6d53eed9bddedbcc3994
Venkatesh Hodavdekar <mail.dizzy@gmail.com>
2012-06-06 21:37:52 -0400

    clean up unwanted code

--- src/com/java/gwt/libertycinema/client/BodyPanel.java
+++ src/com/java/gwt/libertycinema/client/BodyPanel.java
@@ -1,18 +1,16 @@
 package com.java.gwt.libertycinema.client;
 
 
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 public class BodyPanel extends VerticalPanel {
 
     public BodyPanel() {
-	super();
-	this.add(new HTML ("test panel"));
+        super();
     }
 
     public BodyPanel getBodyPanel() {
-	return this;
+        return this;
     }
 }
