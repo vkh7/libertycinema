9bea4f07b5fd0ba6f70c516b6a589a9763abd12a
Venkatesh Hodavdekar <mail.dizzy@gmail.com>
2012-06-08 08:58:21 -0400

    * add new baselayout composite * use base layout in entry point

--- src/com/java/gwt/libertycinema/client/LibertyCinema.java
+++ src/com/java/gwt/libertycinema/client/LibertyCinema.java
@@ -4,12 +4,10 @@ package com.java.gwt.libertycinema.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 
-import com.java.gwt.libertycinema.client.MainPanel;
-
 
 public class LibertyCinema implements EntryPoint {
 
     public void onModuleLoad() {
-        RootLayoutPanel.get().add(new MainPanel());
+        RootLayoutPanel.get().add(new BaseLayout());
     }
 }
