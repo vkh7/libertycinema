cb7420b00f41e4c1ef8018fa36f29fc380401722
Venkatesh Hodavdekar <mail.dizzy@gmail.com>
2012-06-08 08:34:01 -0400

    main panel should be a composite

--- src/com/java/gwt/libertycinema/client/MainPanel.java
+++ src/com/java/gwt/libertycinema/client/MainPanel.java
@@ -1,13 +1,16 @@
 package com.java.gwt.libertycinema.client;
 
 
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.ScrollPanel;
 
 
-public class MainPanel extends VerticalPanel {
+public class MainPanel extends Composite {
+
+    private ScrollPanel contentHolder = new ScrollPanel();
 
     public MainPanel() {
-        super();
+        initWidget(contentHolder);
     }
 
     public MainPanel getBodyPanel() {
