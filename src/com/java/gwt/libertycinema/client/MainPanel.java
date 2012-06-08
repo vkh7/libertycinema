fc25f3dd58f36440b933fa3d5054d81c18889412
Venkatesh Hodavdekar <mail.dizzy@gmail.com>
2012-06-08 09:06:23 -0400

    finalize simple structure of base layout

--- src/com/java/gwt/libertycinema/client/MainPanel.java
+++ src/com/java/gwt/libertycinema/client/MainPanel.java
@@ -7,13 +7,23 @@ import com.google.gwt.user.client.ui.ScrollPanel;
 
 public class MainPanel extends Composite {
 
+    private BaseLayout baseLayout;
     private ScrollPanel contentHolder = new ScrollPanel();
 
-    public MainPanel() {
+    public MainPanel(BaseLayout baseLayout) {
+        this.setBaseLayout(baseLayout);
         initWidget(contentHolder);
     }
 
     public MainPanel getBodyPanel() {
         return this;
+    }
+
+    public BaseLayout getBaseLayout() {
+        return baseLayout;
+    }
+
+    public void setBaseLayout(BaseLayout baseLayout) {
+        this.baseLayout = baseLayout;
     }
 }
