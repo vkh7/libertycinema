67bf12bfc53481613dda1cbb1366f1a6102eec20
Venkatesh Hodavdekar <mail.dizzy@gmail.com>
2012-06-09 10:10:29 -0400

    add validation to image upload view

--- src/com/java/gwt/libertycinema/client/views/ImageUpload.java
+++ src/com/java/gwt/libertycinema/client/views/ImageUpload.java
@@ -5,24 +5,23 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.FileUpload;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
 import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
 
 import com.java.gwt.libertycinema.client.BaseLayout;
-import com.java.gwt.libertycinema.client.MainPanel;
 
 
 public class ImageUpload implements Command {
 
     private final BaseLayout baseLayout;
     private final FormPanel form = new FormPanel();
+    private final Label errorText = new Label();
 
     public ImageUpload(BaseLayout baseLayout) {
         this.baseLayout = baseLayout;
@@ -32,46 +31,49 @@ public class ImageUpload implements Command {
         form.setAction("/libertycinema/putimage");
         form.setEncoding(FormPanel.ENCODING_MULTIPART);
         form.setMethod(FormPanel.METHOD_POST);
-        
-        Grid grid = new Grid(4, 2);
-        
-        grid.setWidget(0, 0, new Label("Title:"));
+
+        FlexTable grid = new FlexTable();
+        grid.setStyleName("formTable");
+
+        grid.setWidget(1, 0, new Label("Title:"));
         final TextBox title = new TextBox();
         title.setName("title");
-        grid.setWidget(0, 1, title);
-        
-        grid.setWidget(1, 0, new Label("Description:"));
+        grid.setWidget(1, 1, title);
+        grid.setWidget(1, 2, errorText);
+        errorText.setStyleName("validationText");
+
+        grid.setWidget(2, 0, new Label("Description:"));
         final TextArea description = new TextArea();
         description.setName("description");
-        grid.setWidget(1, 1, description);
-        
-        grid.setWidget(2, 0, new Label("Pick a file:"));
+        grid.setWidget(2, 1, description);
+
+        grid.setWidget(3, 0, new Label("Pick a file:"));
         FileUpload upload = new FileUpload();
         upload.setName("image");
-        grid.setWidget(2, 1, upload);
-        
-        grid.setWidget(3, 1, new Button("Submit", new ClickHandler() {
+        grid.setWidget(3, 1, upload);
+
+        grid.setWidget(4, 1, new Button("Upload Image", new ClickHandler() {
             public void onClick(ClickEvent event) {
-                    form.submit();
+                form.submit();
             }
         }));
-        
-        form.setWidget(grid);
-        
+
         form.addSubmitHandler(new FormPanel.SubmitHandler() {
             public void onSubmit(SubmitEvent event) {
                 if (title.getText().length() == 0) {
+                    errorText.setText("Please fill in a title string");
                     event.cancel();
-                    }
                 }
-        });
-        
-        form.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
-            public void onSubmitComplete(SubmitCompleteEvent event) {
-        
             }
         });
-        
+
+        form.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
+            public void onSubmitComplete(SubmitCompleteEvent event) {
+            }
+        });
+
+        form.setWidget(grid);
+
         baseLayout.getMainPanel().setTitle("Image Uploads");
         baseLayout.getMainPanel().setContent(form);
     }
