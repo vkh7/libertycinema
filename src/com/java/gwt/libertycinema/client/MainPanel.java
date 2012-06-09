336434ed8a83706043dfba4e2d5690b906ae21f6
Venkatesh Hodavdekar <mail.dizzy@gmail.com>
2012-06-08 23:33:07 -0400

    get basic structure of MenuPanel

--- src/com/java/gwt/libertycinema/client/MainPanel.java
+++ src/com/java/gwt/libertycinema/client/MainPanel.java
@@ -2,21 +2,30 @@ package com.java.gwt.libertycinema.client;
 
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 
 public class MainPanel extends Composite {
 
     private BaseLayout baseLayout;
     private ScrollPanel contentHolder = new ScrollPanel();
+    private Label title = new Label();
 
     public MainPanel(BaseLayout baseLayout) {
+        title.setStyleName("contentTitle");
         this.setBaseLayout(baseLayout);
         initWidget(contentHolder);
     }
 
-    public MainPanel getBodyPanel() {
-        return this;
+    public void setTitle(String title) {
+        this.title.setText(title);
+    }
+
+    public void setContent(Widget widget) {
+        contentHolder.clear();
+        contentHolder.setWidget(widget);
     }
 
     public BaseLayout getBaseLayout() {
