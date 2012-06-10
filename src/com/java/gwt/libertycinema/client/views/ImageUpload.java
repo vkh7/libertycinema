22ad62d419c9e009de17373bf7e93b3ac4600e6f
Venkatesh Hodavdekar <mail.dizzy@gmail.com>
2012-06-10 01:40:30 -0400

    add more validation to image uploads

--- src/com/java/gwt/libertycinema/client/views/ImageUpload.java
+++ src/com/java/gwt/libertycinema/client/views/ImageUpload.java
@@ -21,7 +21,9 @@ public class ImageUpload implements Command {
 
     private final BaseLayout baseLayout;
     private final FormPanel form = new FormPanel();
-    private final Label errorText = new Label();
+    private FileUpload upload = new FileUpload();
+    private final Label errorTitle = new Label();
+    private final Label errorImage = new Label();
 
     public ImageUpload(BaseLayout baseLayout) {
         this.baseLayout = baseLayout;
@@ -39,8 +41,8 @@ public class ImageUpload implements Command {
         final TextBox title = new TextBox();
         title.setName("title");
         grid.setWidget(1, 1, title);
-        grid.setWidget(1, 2, errorText);
-        errorText.setStyleName("validationText");
+        grid.setWidget(1, 2, errorTitle);
+        errorTitle.setStyleName("validationError");
 
         grid.setWidget(2, 0, new Label("Description:"));
         final TextArea description = new TextArea();
@@ -48,9 +50,10 @@ public class ImageUpload implements Command {
         grid.setWidget(2, 1, description);
 
         grid.setWidget(3, 0, new Label("Pick a file:"));
-        FileUpload upload = new FileUpload();
         upload.setName("image");
         grid.setWidget(3, 1, upload);
+        grid.setWidget(3, 2, errorImage);
+        errorImage.setStyleName("validationError");
 
         grid.setWidget(4, 1, new Button("Upload Image", new ClickHandler() {
             public void onClick(ClickEvent event) {
@@ -61,7 +64,10 @@ public class ImageUpload implements Command {
         form.addSubmitHandler(new FormPanel.SubmitHandler() {
             public void onSubmit(SubmitEvent event) {
                 if (title.getText().length() == 0) {
-                    errorText.setText("Please fill in a title string");
+                    errorTitle.setText("Please fill in a title string");
+                    event.cancel();
+                } else if (upload.getFilename().equals("")) {
+                    errorImage.setText("Please pick a file");
                     event.cancel();
                 }
             }
@@ -69,6 +75,7 @@ public class ImageUpload implements Command {
 
         form.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
             public void onSubmitComplete(SubmitCompleteEvent event) {
+                event.getResults();
             }
         });
 
